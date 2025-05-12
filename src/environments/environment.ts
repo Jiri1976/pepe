8a6e278cd6a5e460f9cc1a771c3bb0dc42d5d1ae
EU\kubicjir <jiri.kubiczek@tietoevry.com>
2025-05-12 14:35:05 +0200

    for vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     // SHIFTS_PATH: 'http://localhost:5005/api/',
     // WAREHOUSE_PATH: 'http://localhost:5010/api/'
 
-    AUTHENTICATION_PATH: 'http://pepe-authentication.runasp.net/api/',
-    SHIFTS_PATH: 'http://pepe-shifts.runasp.net/api/',
-    WAREHOUSE_PATH: 'http://pepe-warehouse.runasp.net/api/'
+    AUTHENTICATION_PATH: 'https://pepe-authentication.runasp.net/api/',
+    SHIFTS_PATH: 'https://pepe-shifts.runasp.net/api/',
+    WAREHOUSE_PATH: 'https://pepe-warehouse.runasp.net/api/'
 };
